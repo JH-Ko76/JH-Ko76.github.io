70e25418c7e4c2b26851490ad6f4c0fde3d52d7a
JH-Ko <72744580+JH-Ko76@users.noreply.github.com>
2024-02-26 15:42:59 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+/* 
+let a = 10;
+let b = 100;
+//export {} 로 여러개 가져오는경우는 원하는대로 작명을 못함 
+//하나만 가져오는 경우 원하는대로 작명가능 
+export default a;
+*/
+
+let data = [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000
+    }
+  ] 
+
+export default data;
